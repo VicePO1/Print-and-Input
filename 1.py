8b1a5795cf7e1b7aa7785f39821cfc3f71fc915a
VicePO1 <viclindo11@gmail.com>
2024-09-28 15:28:43 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,4 @@
+n1 = input("primeiro número: ")
+n2 = input("segundo número: ")
+
+print(n1,n2)
---
+++ médias.py
@@ -0,0 +1,5 @@
+n1=int(input("primeiro número:"))
+n2=int(input("segundo número:"))
+media=(n1+n2)/2
+
+print(media)
